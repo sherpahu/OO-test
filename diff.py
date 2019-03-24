3db9109c035c18f8bc4523f0298551b968749718
sherpahu <great_cccp@mail.ru>
2019-03-24 11:01:31 +0800

    Update diff.py

--- diff.py
+++ diff.py
@@ -7,33 +7,22 @@ def main(canshu1,canshu2):
     expected=canshu1
     actual=canshu2
     '''
-    if actual is "WRONG FORMAT!":
-        print("WRONG FORMAT1")
-        exit(0)
-    print(expected)
+    TO-DO:
+    WRONG FORMAT的异常处理
     '''
     expected = expected.replace("^","**")
     actual = actual.replace("^","**")
 
     out=diff(eval(expected))
-    #print(out)
-    #out=eval(expected)
+    #print(out)#视情况输出
     ans=eval(actual)
 
     print(simplify(simplify(out)-simplify(ans)) == 0 ,end="")
     print(simplify(out-ans) == 0,end="")
     print(out.equals(ans))
-
-    #print(float(out.subs(x, 7)))
-    #print(float(ans.subs(x,7)))
-
 main(sys.argv[1],sys.argv[2])
 
-
-
-
-
-
+#完全模拟评测方法, 效果不好
 '''
 numList=[-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10]
 for i in range(1000):
